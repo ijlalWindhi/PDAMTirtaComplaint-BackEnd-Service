e59abe87de729ec0bd208e8e1b8228ebb1f4c14f
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-06-26 14:22:12 +0700

    change port

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ app.use('/user', user);
 app.use('/report', report);
 
 //run server
-// const port = process.env.PORT || 2004;
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 2004;
 
 app.listen(PORT, () => {
     console.log('server run on port ' + PORT);
