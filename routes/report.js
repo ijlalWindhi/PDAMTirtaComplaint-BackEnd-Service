4b65de7cd672a5f01bea588e4a7b4cd43c549a78
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-06-26 11:41:18 +0700

    initial library

---
+++ routes/report.js
@@ -0,0 +1,30 @@
+//import library
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//implementasi library
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//import multer
+const multer = require("multer")
+const path = require("path")
+const fs = require("fs")
+
+//import model
+const model = require('../models/index');
+const report = model.report
+
+//config storage image
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/image/userProfile")
+    },
+    filename: (req, file, cb) => {
+        cb(null, "img-" + Date.now() + path.extname(file.originalname))
+    }
+})
+let upload = multer({ storage: storage })
+
+module.export = app
